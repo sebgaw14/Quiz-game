a4a894c69e1c15deeea9f381f178815ae88adba9
sebgaw12 <sebastian.gawrys1@gmail.com>
2023-10-21 12:41:12 +0200

    routing, landing page

---
+++ src/LandingPage.tsx
@@ -0,0 +1,12 @@
+import {Container} from "react-bootstrap";
+
+const LandingPage = () => {
+    console.log("landing page");
+  return (
+      <Container>
+        <h1>Welcome</h1>
+      </Container>
+  )
+}
+
+export default LandingPage
